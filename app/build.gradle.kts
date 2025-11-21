00314ab6de56a588fec1724aa51e31e0dd969167
GMB8281 <gabrielbaraglio@outlook.com.br>
2025-11-21 15:39:14 -0300

    2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.3"
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
