11ff5e387e0d5e22298d1706e13c22876587c817
GMB8281 <gabrielbaraglio@outlook.com.br>
2025-11-24 17:27:04 -0300

    5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "5.2"
+        versionName = "5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
